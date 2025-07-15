36c80a724c24cc881ab41c0bd4810be8890f610e
Sebastian Stoelen <gewoonseba@gmail.com>
2025-07-15 08:46:58 +0200

    Setup starting styles version of toaster

---
+++ app/exercises/toast-component-starting-styles/component.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import styles from "./component.module.css";
+
+interface ToastProps {
+  index: number;
+}
+
+function Toast({ index }: ToastProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div
+      className={styles.toast}
+      style={{ "--index": index } as React.CSSProperties}
+      data-mounted={mounted}
+    >
+      <span className={styles.title}>Event Created</span>
+      <span className={styles.description}>Monday, January 3rd at 6:00pm</span>
+    </div>
+  );
+}
+
+export function ToastComponentStartingStylesComponent() {
+  const [toasts, setToasts] = useState(0);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.toaster}>
+        {Array.from({ length: toasts }).map((_, i) => (
+          <Toast key={i} index={toasts - i - 1} />
+        ))}
+      </div>
+      <button
+        className={styles.button}
+        onClick={() => {
+          setToasts(toasts + 1);
+        }}
+      >
+        Add toast
+      </button>
+    </div>
+  );
+}
