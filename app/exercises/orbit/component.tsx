d4f0f7e5b70aea656d6ff47b8937e5f69c029a5c
Sebastian Stoelen <gewoonseba@gmail.com>
2025-07-22 21:36:13 +0200

    Finsish orbit exercise

--- app/exercises/orbit/component.tsx
+++ app/exercises/orbit/component.tsx
@@ -2,7 +2,7 @@ import styles from "./component.module.css";
 
 export function Orbit() {
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.circle} />
       <div className={styles.orbitingCircle} />
     </div>
