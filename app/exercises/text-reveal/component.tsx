6333f462e5816beca61d684484e02f70b0e2888a
Sebastian Stoelen <gewoonseba@gmail.com>
2025-07-22 20:05:24 +0200

    Work in progress

--- app/exercises/text-reveal/component.tsx
+++ app/exercises/text-reveal/component.tsx
@@ -6,10 +6,21 @@ import styles from "./component.module.css";
 export function TextReveal() {
   const [reset, setReset] = useState(0);
 
+  const WORD = "Animations";
+
   return (
     <div>
       <div key={reset}>
-        <h1 className={styles.h1}>Animations</h1>
+        <h1 className={styles.h1}>
+          {WORD.split("").map((char, index) => (
+            <span
+              key={index}
+              style={{ "--index": index } as React.CSSProperties}
+            >
+              {char}
+            </span>
+          ))}
+        </h1>
       </div>
       {/* Use this button to replay your animation */}
       <button className={styles.button} onClick={() => setReset(reset + 1)}>
