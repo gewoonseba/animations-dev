0b1ab462cb10b3e6e7cd637ece36aa9902e32c03
Sebastian Stoelen <seba@gewoonseba.com>
2025-08-11 20:26:57 +0200

    Fixed exit animation for form

--- app/exercises/feedback-component/component.tsx
+++ app/exercises/feedback-component/component.tsx
@@ -85,88 +85,91 @@ export function FeedbackComponent() {
             >
               Feedback
             </motion.span>
-            {formState === 'success' ? (
-              <motion.div
-                animate={{ y: 0, filter: 'blur(0px)' }}
-                className={styles.successWrapper}
-                initial={{ y: -15, filter: 'blur(12px)' }}
-                transition={{
-                  type: 'spring',
-                  duration: 0.6,
-                  bounce: 0,
-                }}
-              >
-                <svg
-                  fill="none"
-                  height="32"
-                  viewBox="0 0 32 32"
-                  width="32"
-                  xmlns="http://www.w3.org/2000/svg"
+            <AnimatePresence mode="popLayout">
+              {formState === 'success' ? (
+                <motion.div
+                  animate={{ y: 0, filter: 'blur(0px)' }}
+                  className={styles.successWrapper}
+                  initial={{ y: -32, filter: 'blur(4px)' }}
+                  key="success"
+                  transition={popOverTransition}
                 >
-                  <title>Success</title>
-                  <path
-                    d="M27.6 16C27.6 17.5234 27.3 19.0318 26.717 20.4392C26.1341 21.8465 25.2796 23.1253 24.2025 24.2025C23.1253 25.2796 21.8465 26.1341 20.4392 26.717C19.0318 27.3 17.5234 27.6 16 27.6C14.4767 27.6 12.9683 27.3 11.5609 26.717C10.1535 26.1341 8.87475 25.2796 7.79759 24.2025C6.72043 23.1253 5.86598 21.8465 5.28302 20.4392C4.70007 19.0318 4.40002 17.5234 4.40002 16C4.40002 12.9235 5.62216 9.97301 7.79759 7.79759C9.97301 5.62216 12.9235 4.40002 16 4.40002C19.0765 4.40002 22.027 5.62216 24.2025 7.79759C26.3779 9.97301 27.6 12.9235 27.6 16Z"
-                    fill="#2090FF"
-                    fillOpacity="0.16"
+                  <svg
+                    fill="none"
+                    height="32"
+                    viewBox="0 0 32 32"
+                    width="32"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Success</title>
+                    <path
+                      d="M27.6 16C27.6 17.5234 27.3 19.0318 26.717 20.4392C26.1341 21.8465 25.2796 23.1253 24.2025 24.2025C23.1253 25.2796 21.8465 26.1341 20.4392 26.717C19.0318 27.3 17.5234 27.6 16 27.6C14.4767 27.6 12.9683 27.3 11.5609 26.717C10.1535 26.1341 8.87475 25.2796 7.79759 24.2025C6.72043 23.1253 5.86598 21.8465 5.28302 20.4392C4.70007 19.0318 4.40002 17.5234 4.40002 16C4.40002 12.9235 5.62216 9.97301 7.79759 7.79759C9.97301 5.62216 12.9235 4.40002 16 4.40002C19.0765 4.40002 22.027 5.62216 24.2025 7.79759C26.3779 9.97301 27.6 12.9235 27.6 16Z"
+                      fill="#2090FF"
+                      fillOpacity="0.16"
+                    />
+                    <path
+                      d="M12.1334 16.9667L15.0334 19.8667L19.8667 13.1M27.6 16C27.6 17.5234 27.3 19.0318 26.717 20.4392C26.1341 21.8465 25.2796 23.1253 24.2025 24.2025C23.1253 25.2796 21.8465 26.1341 20.4392 26.717C19.0318 27.3 17.5234 27.6 16 27.6C14.4767 27.6 12.9683 27.3 11.5609 26.717C10.1535 26.1341 8.87475 25.2796 7.79759 24.2025C6.72043 23.1253 5.86598 21.8465 5.28302 20.4392C4.70007 19.0318 4.40002 17.5234 4.40002 16C4.40002 12.9235 5.62216 9.97301 7.79759 7.79759C9.97301 5.62216 12.9235 4.40002 16 4.40002C19.0765 4.40002 22.027 5.62216 24.2025 7.79759C26.3779 9.97301 27.6 12.9235 27.6 16Z"
+                      stroke="#2090FF"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2.4"
+                    />
+                  </svg>
+                  <h3>Feedback received!</h3>
+                  <p>Thanks for helping me improve Sonner.</p>
+                </motion.div>
+              ) : (
+                <motion.form
+                  animate={{ y: 0, opacity: 1, filter: 'blur(0px)' }}
+                  className={styles.feedbackForm}
+                  exit={{ y: 32, opacity: 0, filter: 'blur(4px)' }}
+                  key="form"
+                  onSubmit={(event) => {
+                    event.preventDefault();
+                    if (!feedback) {
+                      return;
+                    }
+                    submit();
+                  }}
+                  transition={popOverTransition}
+                >
+                  <textarea
+                    autoFocus
+                    className={styles.textarea}
+                    onChange={(event) => setFeedback(event.target.value)}
+                    placeholder="Feedback"
+                    required
                   />
-                  <path
-                    d="M12.1334 16.9667L15.0334 19.8667L19.8667 13.1M27.6 16C27.6 17.5234 27.3 19.0318 26.717 20.4392C26.1341 21.8465 25.2796 23.1253 24.2025 24.2025C23.1253 25.2796 21.8465 26.1341 20.4392 26.717C19.0318 27.3 17.5234 27.6 16 27.6C14.4767 27.6 12.9683 27.3 11.5609 26.717C10.1535 26.1341 8.87475 25.2796 7.79759 24.2025C6.72043 23.1253 5.86598 21.8465 5.28302 20.4392C4.70007 19.0318 4.40002 17.5234 4.40002 16C4.40002 12.9235 5.62216 9.97301 7.79759 7.79759C9.97301 5.62216 12.9235 4.40002 16 4.40002C19.0765 4.40002 22.027 5.62216 24.2025 7.79759C26.3779 9.97301 27.6 12.9235 27.6 16Z"
-                    stroke="#2090FF"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2.4"
-                  />
-                </svg>
-                <h3>Feedback received!</h3>
-                <p>Thanks for helping me improve Sonner.</p>
-              </motion.div>
-            ) : (
-              <form
-                className={styles.feedbackForm}
-                onSubmit={(event) => {
-                  event.preventDefault();
-                  if (!feedback) {
-                    return;
-                  }
-                  submit();
-                }}
-              >
-                <textarea
-                  autoFocus
-                  className={styles.textarea}
-                  onChange={(event) => setFeedback(event.target.value)}
-                  placeholder="Feedback"
-                  required
-                />
-                <div className={styles.feedbackFooter}>
-                  <Divider />
-                  <button className={styles.submitButton} type="submit">
-                    <AnimatePresence initial={false} mode="popLayout">
-                      <motion.span
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: 25 }}
-                        initial={{ opacity: 0, y: -25 }}
-                        key={formState}
-                        transition={{
-                          type: 'spring',
-                          duration: 0.3,
-                          bounce: 0,
-                        }}
-                      >
-                        {formState === 'loading' ? (
-                          <Spinner
-                            color="rgba(255, 255, 255, 0.65)"
-                            size={14}
-                          />
-                        ) : (
-                          <span>Send feedback</span>
-                        )}
-                      </motion.span>
-                    </AnimatePresence>
-                  </button>
-                </div>
-              </form>
-            )}
+                  <div className={styles.feedbackFooter}>
+                    <Divider />
+                    <button className={styles.submitButton} type="submit">
+                      <AnimatePresence initial={false} mode="popLayout">
+                        <motion.span
+                          animate={{ opacity: 1, y: 0 }}
+                          exit={{ opacity: 0, y: 25 }}
+                          initial={{ opacity: 0, y: -25 }}
+                          key={formState}
+                          transition={{
+                            type: 'spring',
+                            duration: 0.3,
+                            bounce: 0,
+                          }}
+                        >
+                          {formState === 'loading' ? (
+                            <Spinner
+                              color="rgba(255, 255, 255, 0.65)"
+                              size={14}
+                            />
+                          ) : (
+                            <span>Send feedback</span>
+                          )}
+                        </motion.span>
+                      </AnimatePresence>
+                    </button>
+                  </div>
+                </motion.form>
+              )}
+            </AnimatePresence>
           </motion.div>
         ) : null}
       </AnimatePresence>
